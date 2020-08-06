aef2cad10069a60444e953e94050d2da3ba56c54
Ube Halaya <eli.tamondong@gmail.com>
2020-08-06 15:40:37 -0400

    canvas functionality added

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,6 +1,8 @@
-import React, {useEffect} from 'react'
-import * as p5 from 'p5'
+import React, {useState, useEffect} from 'react'
+import p5 from 'p5'
 import Matter from 'matter-js'
+
+import {setupWorld} from './constructors'
 
 const {Engine, Events, World} = Matter
 
@@ -9,21 +11,34 @@ const Canvas = props => {
   const engine = Engine.create()
   const world = engine.world
 
+  const bodies = []
+
   const Sketch = p5 => {
     let width = window.innerWidth
     let height = window.innerHeight
+    // const {setup, draw, createCanvas} = p5
+
+    const settings = {p5, engine, world}
+    const viewScreen = {width, height}
 
     p5.setup = () => {
       p5.createCanvas(width, height)
+      setupWorld(settings, viewScreen, bodies)
     }
     p5.draw = () => {
-      // p5.background(50, 50, 50, 150)
-      p5.push()
-      p5.rectMode(p5.CENTER)
-      p5.strokeWeight(1)
-      p5.stroke(255)
-      p5.rect(200, 200, 200, 200)
-      p5.pop()
+      p5.background(50, 50, 50)
+      Engine.update(engine)
+      if (bodies.length) {
+        for (let i = 0; i < bodies.length; i++) {
+          bodies[i].show()
+        }
+      }
+      // p5.push()
+      // p5.rectMode(p5.CENTER)
+      // p5.strokeWeight(1)
+      // p5.stroke(255)
+      // p5.rect(200, 200, 200, 200)
+      // p5.pop()
     }
   }
 
--- src/components/constructors.js
+++ src/components/constructors.js
@@ -1,9 +1,11 @@
 import Matter from 'matter-js'
+// import p5 from 'p5'
 const {Bodies, World} = Matter
 
-export const boxConstructor = settings => {
-  // const {p5, world} = settings
-  return function Box(x, y, w, h, color) {
+const textBoxConstructor = settings => {
+  const {p5, world} = settings
+  // const {push, translate, rotate, rectMode, textAlign, text, CENTER, fill, pop} = p5
+  return function TextBox(x, y, w, h, inputText) {
     const options = {
       friction: 0.4,
       restitution: 0.8,
@@ -11,38 +13,32 @@ export const boxConstructor = settings => {
     this.body = Bodies.rectangle(x, y, w, h, options)
     this.w = w
     this.h = h
-    this.id = this.body.id
-    // this.hue = color.hue
-    // this.saturation = color.saturation
-    // this.lightness = color.lightness
-    // this.alpha = color.alpha
+    // this.id = this.body.id
 
-    // this.isOffScreen = function() {
-    //   return this.body.position.y > p5.windowHeight
-    // }
+    this.show = () => {
+      this.pos = this.body.position
+      this.angle = this.body.angle
+      p5.push()
+      p5.translate(this.pos.x, this.pos.y)
+      p5.rotate(this.angle)
 
-    // this.removeFromWorld = function() {
-    //   World.remove(world, this.body)
-    // }
-
-    // this.show = function() {
-    //   this.pos = this.body.position
-    //   this.angle = this.body.angle
-    //   p5.push()
-    //   p5.translate(this.pos.x, this.pos.y)
-    //   p5.rotate(this.angle)
-    //   p5.rectMode(p5.CENTER)
-    //   p5.colorMode(p5.HSL)
-    //   p5.fill(this.hue, this.saturation, this.lightness, this.alpha)
-    //   p5.strokeWeight(1)
-    //   p5.stroke(this.hue, 100, 65, 100)
-    //   p5.rect(0, 0, this.w, this.h)
-    //   p5.pop()
-    // }
+      p5.rectMode(p5.CENTER)
+      p5.textAlign(p5.CENTER, p5.CENTER)
+      p5.fill(255)
+      p5.textSize(64)
+      p5.text(inputText, 0, 0)
+      // p5.colorMode(p5.HSL)
+      // p5.fill(0, 50, 100, 0.2)
+      // // p5.fill(this.hue, this.saturation, this.lightness, this.alpha)
+      // p5.strokeWeight(1)
+      // p5.stroke(150, 100, 65, 50)
+      // p5.rect(0, 0, this.w, this.h)
+      p5.pop()
+    }
   }
 }
 
-export const boundaryConstructor = settings => {
+const boundaryConstructor = settings => {
   return function Boundary(x, y, w, h, label = 'boundary') {
     const options = {
       friction: 0.3,
@@ -54,4 +50,24 @@ export const boundaryConstructor = settings => {
     this.w = w
     this.h = h
   }
+}
+
+export const setupWorld = (settings, viewScreen, bodies) => {
+  const {p5, world} = settings
+  const {width, height} = viewScreen
+  const TextBox = textBoxConstructor(settings)
+  const Boundary = boundaryConstructor(settings)
+  // const bodies = bodies
+
+  let ground = new Boundary(width / 2, height + 25, width, 50)
+  let titleText = 'hello world'
+  // const letters = []
+  for (let i = 0; i < titleText.length; i++) {
+    let letter = new TextBox(width / 2, height / 5, 50, 50, titleText[i])
+    World.add(world, letter.body)
+    bodies.push(letter)
+  }
+  // let title = new TextBox(width / 2, height / 5, 50, 50, 'hello world')
+  World.add(world, ground.body)
+  // bodies.push(title)
 }
